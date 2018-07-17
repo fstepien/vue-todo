f5910ffb6f1e040f0f4bf9b057eda36c5c912dfe
Filip Stepien <contact@filipstepien.com>
2018-07-17 17:03:47 -0400

    include render_newTodos to listner function

--- client/client.js
+++ client/client.js
@@ -1,32 +1,5 @@
 const server = io({ transports: ["websocket"], upgrade: false });
-
-function add() {
-  // console.warn(event);
-  const input = document.getElementById("todo-input");
-
-  // Emit the new todo as some data to the server
-  server.emit("make", {
-    title: input.value
-  });
-
-  // Clear the input
-  input.value = "";
-  // TODO: refocus the element
-}
-
-function render(todo) {
-  const listItem = document.createElement("li");
-  const listItemText = document.createTextNode(todo.title);
-  listItem.appendChild(listItemText);
-  list.append(listItem);
-}
-
-// NOTE: These are listeners for events from the server
-// This event is for (re)loading the entire list of todos from the server
-
-// server.on("render_newTodo", todo => {
-//   render(todo);
-// });
+// const server = io.connect("http://localhost:3003/");
 
 const app = new Vue({
   el: "#app",
@@ -45,6 +18,9 @@ const app = new Vue({
       console.log(server);
       server.on("load", ({ todos, id }) => {
         this.todos = todos;
+      });
+      server.on("render_newTodo", todo => {
+        this.todos.push(todo);
       });
     },
     add() {
